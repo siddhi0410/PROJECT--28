091f94478f9fcdd61a3871b0bc36f7d14f85175d
siddhi0410 <81573606+siddhi0410@users.noreply.github.com>
2021-04-30 16:17:39 +0530

    Add files via upload

---
+++ Project-C28-template-main/Project-C28-template-main/js/launcher.js
@@ -0,0 +1,28 @@
+class Launcher{
+    constructor(bodyA,pointB){
+       var options = {
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.04,
+        length:10
+       } 
+       this.pointB = pointB;
+       this.launch = Constraint.create(options)
+       World.add(world,this.launch)
+    }
+    attach(body)
+        {this.launch.bodyA=body}
+    fly(){
+        this.launch.bodyA = null;
+    }
+    display(){
+        if(this.launch.bodyA){
+            
+            var pointA = this.launch.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3)
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+       
+    }
+}
